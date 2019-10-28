a6164b432a47ceb41ddcd25697ab6a0bf9911d22
mplichta <bigcatplichta@gmail.com>
2019-10-28 12:43:14 -0600

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    # attr_accessor :title, :content
+
+    # def initialize(params)
+    #     params.each do |key, value|
+    #         self.send("#{key}=", value)
+    #     end
+    # end
+
 end
